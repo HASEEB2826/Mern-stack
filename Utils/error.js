f8a1c1f834ce9cb2bf70d5457e5e5dd81c876bfc
HASEEB2826 <87098436+HASEEB2826@users.noreply.github.com>
2024-07-20 14:31:11 +0500

    Delete Utils directory

--- Utils/error.js
+++
@@ -1,6 +0,0 @@
-export const errorHandler = (Statuscode, message) => {
-  const err = new Error();
-  err.Statuscode = Statuscode || err.Statuscode;
-  err.message = message || err.message;
-  return err;
-};
